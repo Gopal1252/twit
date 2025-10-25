32dcf344e7662dae2f9ca9f9001bc3559769f01f
Gopal1252 <guptagopal252@gmail.com>
2025-10-25 17:14:12 +0530

    added init command

--- src/main/java/com/gopal/twit/commands/InitCommand.java
+++ src/main/java/com/gopal/twit/commands/InitCommand.java
@@ -1,8 +1,14 @@
 package com.gopal.twit.commands;
 
+import com.gopal.twit.core.GitRepository;
+
+import java.nio.file.Paths;
+
 public class InitCommand implements Command{
     @Override
     public void execute(String[] args) throws Exception {
-
+        String path = args.length > 0 ? args[0] : ".";
+        GitRepository.create(Paths.get(path));
+        System.out.println("Initialized an empty Git repository in " + path);
     }
 }
